3955d3bafdf5ea9fa8ae5c21c9e55f58e1c96bd0
abdharris26 <abdharris246@gmail.com>
2026-02-25 23:36:58 +0700

    studi kasus try exception

---
+++ Abdul-Harris_25071101559/pertemuan1/tugas_praktikum/soal3.py
@@ -0,0 +1,8 @@
+# Soal 3: If Else
+
+nilai_ujian = 40
+
+if nilai_ujian >= 70:
+    print("Lulus")
+else:
+    print("Tidak Lulus")
---
+++ Abdul-Harris_25071101559/pertemuan4/latihan/except.py
@@ -0,0 +1,19 @@
+class NilaiMahasiswa:
+    def hitung_rata_rata(self):
+        try:
+            nilai1 = float(input("masukan nilai 1: "))
+            nilai2 = float(input("masukan nilai kedua: "))
+            nilai3 = float(input("masukan nilai ketiga: "))
+            
+            rata_rata = (nilai1 + nilai2 + nilai3)/3
+            print("rata-rata nilainya: ",rata_rata)
+            
+        except valueError:
+            print("input harus berupa angka")
+        
+        finally:
+            print("program telah selesai")
+            
+objek = NilaiMahasiswa()
+objek.hitung_rata_rata()
+        
